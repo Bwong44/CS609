2ced63c63c36d5c29df0de103848b123f22ce485
Brandon Wong <bwong1086@sdsu.edu>
2026-02-22 23:41:10 -0800

    Added comments and left and right search.

--- hw2/wong_assignment2.py
+++ hw2/wong_assignment2.py
@@ -19,29 +19,58 @@ Output: List of postions where the read is found
 # Posted by sam46
 # Retrieved 2026-02-22, License - CC BY-SA 4.0
 
-s = 'banana'
-find = 'ana'
+string = 'ananaasnaaaaaaaaaana'
+fining = 'ana'
 def readFasta(filename): #Reads the fasta file and returns the sequence as a string
-    """Reads a FASTA file and returns the sequence as a string."""
+    """Reads a FASTA file and returns the sequence as a string"""
     #TODO: Biopython
     pass
 
 def suffixArray(s): #s object bcomes the sorted list object
-    """Builds a suffix array for the given string s."""
-    sa = sorted(range(len(s)), key=lambda i: s[i:])
+    """Builds a suffix array for the given string s"""
+    sa = sorted(range(len(s)), key=lambda i: s[i:]) #Takes index of the string at i and sorts it by the suffix
     return list(sa)
 
 
 
-def binarySearch(s, find):
-    sa = suffixArray(s) #Calls suffixArray function
-    print(sa[0]) #Prints the sorted list object
-    return sa #Returns the second element of the sorted list object
+def binarySearchLeft(s, sa, find): 
+    """Finds the lower bound of the suffix array
+    s: input string
+    sa: suffix array of the input string
+    find: the string to find in the suffix array"""
+    left, right = 0, len(sa)
+    m = len(find) #Stores the length of find
+    while left < right:
+        mid = (left + right) // 2
+        i = sa[mid] #Converts sa position to index of genome string
+        if s[i:i+m] < find: #Compares the suffix to the find string  and moves right since its sorted by alphabetical order
+            left = mid + 1 #Everythign to the left of mid is smaller so if it can't find it we must go right
+        else:
+            right = mid #Could be at mid or to the left so we still include mid
+    return left
 
+def binarySearchRight(s, sa, find):
+    """Finds the upper bound of the suffix array
+    s: input string
+    sa: suffix array of the input string
+    find: the string to find in the suffix array"""
+    left, right = 0, len(sa)
+    m = len(find)
+    while left < right:
+        mid = (left + right) // 2
+        i = sa[mid]
+        if s[i:i+m] <= find: #If the suffix is smaller than find we can only go right because everything to the left must be smaller (sorted)
+            left = mid + 1   #Even if equal we move right because we want to find the upper bound
+        else:
+            right = mid #We have already went past so we move left
+    return left
     
 def main(): #main function to run the read search
-    """Runs the read search."""
-    binarySearch(s, find)
+    """Runs the read search"""
+    sa = suffixArray(string)
+    left = binarySearchLeft(string, sa, fining)
+    right = binarySearchRight(string, sa, fining)
+    print(sorted(sa[left:right])) #indexes the suffix array from left to right and sorts it
 
 
 if __name__ == "__main__":    
